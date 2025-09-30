be46c3f724a89e8fa6e7d8b280fab9de196e0f23
LAVAL Alexandre <alexandre.laval@etu.utc.fr>
2025-09-30 14:52:39 +0200

    disable biorxiv tool

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from src.classify import classify_file
 from src.model.model import Model
 if __name__ == "__main__":
     file = "data/hackathon-test.jsonl"
-    model = Model()
+    #for now turn off biorxiv
+    model = Model(enable_biorxiv=False)
     classify_file(file, model, use_agent=True)
